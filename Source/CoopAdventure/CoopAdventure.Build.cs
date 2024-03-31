8f24f146a7f14f7a67dbb2dd523acaf7714b3f69
Karvalho <renato@karvalho.com.br>
2024-03-31 16:28:49 -0300

    Added support for online subsystem and steam

--- Source/CoopAdventure/CoopAdventure.Build.cs
+++ Source/CoopAdventure/CoopAdventure.Build.cs
@@ -8,6 +8,7 @@ public class CoopAdventure : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore",
+			"EnhancedInput", "OnlineSubsystem", "OnlineSubsystemSteam" });
 	}
 }
